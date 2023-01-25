a1b473398e0625a2f3837fd2f88678a982636e96
Andrew Seavers <17SeaveA@fulford.york.sch.uk>
2023-01-25 12:40:24 +0000

    Created a web page to scam users

---
+++ Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a totally copyright free social media company (I promice) server");
+        }
+    }
+}
